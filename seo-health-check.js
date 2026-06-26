ba061d3eb064da8bc39685efb885b5332820c600
flowinglikeabrooke <brooke@omniux.io>
2026-06-26 10:22:13 -0700

    Add files via upload

--- seo-health-check.js
+++ seo-health-check.js
@@ -21,10 +21,7 @@ const PATHS = [
   '/yogacloak-reserve-page.html',
   '/sitemap.xml',
   '/robots.txt',
-  '/hot-yoga-essentials.html',
-  '/what-to-wear-after-hot-yoga.html',
-  '/hot-yoga-towel-alternative.html',
-  '/studio-to-street-yoga-apparel.html'
+  '/hot-yoga-essentials.html'
 ];
 
 export default async function handler(req, res) {
